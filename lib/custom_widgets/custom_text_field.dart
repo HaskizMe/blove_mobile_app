23e16c1e09555291d662747b8b21ab5a0281a86d
Bailey Haskell <Bailey Haskell>
2023-12-12 15:05:25 -0700

    added login animation, text fields, and create account pop up dialog box

---
+++ lib/custom_widgets/custom_text_field.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final double width;
+  final TextEditingController textController;
+  final double height;
+  final String hintText;
+  final bool isObscure;
+  const CustomTextField({
+    super.key,
+    required this.width,
+    required this.textController,
+    required this.height,
+    required this.hintText,
+    required this.isObscure,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextField(
+        cursorColor: Colors.grey,
+        controller: textController,
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: height / 2 - 16, horizontal: 10),
+          focusColor: Colors.black,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: const BorderSide(
+                  color: Colors.black
+              )
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: const BorderSide(
+                  color: Colors.black
+              )
+          ),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
